7115d467aaa3da3c2af367f9c2a40af5c1ad5e88
Marcel Ferreira <147241429+mferreira-7@users.noreply.github.com>
2025-06-05 21:57:06 +0100

    removed all personal details

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username = "glbl.news_"
-password = "Voltz123."
-news_api_key = "e345ea8b01924e68b532f3764e1e8e7c"
-google_api_key = "AIzaSyA3kqDnWyt1u9TskG4xb5R_BdL2aMg6msU"
+username = "ACCOUNT_USERNAME"
+password = "ACCOUNT_PASSWORD"
+news_api_key = "NEWSAPI_API_KEY"
+google_api_key = "GOOGLE_GENAI_API_KEY"
